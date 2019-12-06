8318531c091b1ceccc48b3a74bdbdf0cd0d7ef4c
Emmanuel Efrayn Ulloa Domínguez <master1932@hotmail.com>
2019-12-06 14:21:06 -0400

    Api OwnersResponse

---
+++ MyLeasing.Common/Models/EmailRequest.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyLeasing.Common
+{
+    public class EmailRequest
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+    }
+}
