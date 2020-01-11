06f972c4f53a9a6fdaec9f676ef4c8951395acd1
Emmanuel Efrayn Ulloa Domínguez <master1932@hotmail.com>
2020-01-11 08:59:08 -0400

    Show Property Details

---
+++ MyLeasing.Prism/MyLeasing.Prism/ViewModels/PropertyPegaViewModel.cs
@@ -0,0 +1,36 @@
+﻿using MyLeasing.Common.Models;
+using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+
+namespace MyLeasing.Prism.ViewModels
+{
+    public class PropertyPegaViewModel : ViewModelBase
+    {
+        private PropertyResponse _property;
+        public PropertyPegaViewModel(
+            INavigationService navigationService) : base(navigationService)
+        {
+            Title = "Property";
+        }
+
+        public PropertyResponse Property
+        {
+            get => _property;
+            set => SetProperty(ref _property, value);  
+        }
+        public override void OnNavigatedTo(INavigationParameters parameters)
+        {
+            base.OnNavigatedTo(parameters);
+            if (parameters.ContainsKey("property"))
+            {
+                Property = parameters.GetValue<PropertyResponse>("property");
+                Title = $"Property: {Property.Neighborhood}";
+            }
+        }
+    }
+}
---
+++ MyLeasing.Prism/MyLeasing.Prism/Views/PropertyPega.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace MyLeasing.Prism.Views
+{
+    public partial class PropertyPega : ContentPage
+    {
+        public PropertyPega()
+        {
+            InitializeComponent();
+        }
+    }
+}
